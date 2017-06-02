57ca1d7ad88e16b5eea100a6a5a37a2df64282cb
resslen <robbert159@wp.pl>
2017-06-02 13:54:28 +0200

    git push

--- app/src/main/java/com/example/resslen/projektbdio/RegisterRequest.java
+++ app/src/main/java/com/example/resslen/projektbdio/RegisterRequest.java
@@ -1,8 +1,30 @@
 package com.example.resslen.projektbdio;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Resslen on 02.06.2017.
  */
 
-public class RegisterRequest {
+public class RegisterRequest extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "https://resslen.000webhostapp.com/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String surname, String name, String password, String email, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("nazwisko", surname);
+        params.put("imie", name);
+        params.put("haslo", password);
+        params.put("email", email);
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return params;
+    }
 }
