5a6a73be0f0965db391f18cbaeb282bedc75d4a0
resslen <robbert159@wp.pl>
2017-06-02 13:54:28 +0200

    dodanie obslugi rejestracji

--- app/src/main/java/com/example/resslen/projektbdio/LoginActivity.java
+++ app/src/main/java/com/example/resslen/projektbdio/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.resslen.projektbdio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.LoginFilter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final Button registerLink = (Button) findViewById(R.id.bRegisterHere);
+
+        registerLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
     }
 }
